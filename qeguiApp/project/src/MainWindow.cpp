e4284e72dede578bee847cbcb2a5f4f8d86ce02c
Andrew Starritt <starritt@netspace.net.au>
2018-11-25 23:11:11 +1100

    Make new QECorrelation widget available to QEGui

--- qeguiApp/project/src/MainWindow.cpp
+++ qeguiApp/project/src/MainWindow.cpp
@@ -1677,6 +1677,7 @@ void MainWindow::createActionMaps()
     inbuiltFormMap.insert( "Plotter...",                             ":/qe/gui/forms/Plotter.ui" );
     inbuiltFormMap.insert( "Table...",                               ":/qe/gui/forms/Table.ui" );
     inbuiltFormMap.insert( "PV Load/Save...",                        ":/qe/gui/forms/PVLoadSave.ui" );
+    inbuiltFormMap.insert( "PV Correlation...",                      ":/qe/gui/forms/PVCorrelation.ui" );
     inbuiltFormMap.insert( "Archive Status...",                      ":/qe/gui/forms/ArchiveStatus.ui" );
     inbuiltFormMap.insert( "Archive Name Search...",                 ":/qe/gui/forms/ArchiveNameSearch.ui" );
 
